d9b3736c3cda2923b0b272dc7a0a2cbae1b8daf8
Nizvan Monteón Ricárdez <njr01397@gmail.com>
2024-05-25 20:28:50 -0600

    Feat(Navbar): Add navbar component

---
+++ src/app/components/newTask/TaskForm.tsx
@@ -0,0 +1,7 @@
+export default function TaskForm() {
+    return (
+        <form>
+
+        </form>
+    )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+	return (
+		<div className='flex justify-center w-full h-full py-4 px-8'>
+			<section className='w-full'>
+				<h1 className='text-4xl font-bold'>My Tasks</h1>
+			</section>
+		</div>
+	)
 }
